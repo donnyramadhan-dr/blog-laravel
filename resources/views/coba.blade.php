17cd63f5264d15662962242ee59eb67a16c49798
donnyramadhan-dr <ramadhandonny60@gmail.com>
2020-07-25 19:59:37 +0700

    first commit

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts/myblog')
+
+@section('profil')
+    <header class="masthead bg-primary  text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img style="border-radius: 50%;" class="masthead-avatar mb-5" src="assets/img/WhatsApp Image 202012323.jpeg" alt="" />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Donny Ramadhan</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div style="width: 1000px;" class="divider-custom-line"></div>
+                    <!-- <div class="divider-custom-line"></div>
+                    <div class="divider-custom-line"></div> -->
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Basketballer - BackEnd Developer - Gamers</p>
+            </div>
+    </header>  
+@endsection
---
+++ resources/views/pengalaman.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts/myblog')
+
+@section('experience')
+<section class="page-section bg-primary text-white mb-0 mt-5" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Experience</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ml-auto"><p class="lead"><ul>
+                    <li style="font-size: 24px;">Mempelajari Bahasa PHP</li>
+                    <li style="font-size: 24px;">Mempelajari Bahasa SQL</li>
+                    <li style="font-size: 24px;">Mempelajari HTML,CSS,JS</li>
+                    </ul></p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">
+                    <ul>
+                    <li style="font-size: 22px;">Berpatisipasi Libas Smanli Cup</li>
+                    <li style="font-size: 22px;">Berpatisipasi O2sn cabang Basketball</li>
+                    </ul></p></div>
+                </div>
+            </div>
+        </section>
+@endsection
